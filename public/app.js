2085dc3d91b8b3b0e76db8b862deb174243ff248
Leon Lamle <llamle@gmail.com>
2016-03-09 14:47:47 -0500

    added $routeProvider, encased the config in an annonymous function to protect from global variables

--- public/app.js
+++ public/app.js
@@ -1,4 +1,37 @@
-angular.module('MyApp', ['ngCookies', 'ngResource', 'ngMessages', 'ngRoute', 'mgcrea.ngStrap'])
-    .config(function(){
+/*jslint white:true */
+/*global angular */
+
+var app = angular.module('MyApp', ['ngCookies', 'ngResource', 'ngMessages', 'ngRoute', 'mgcrea.ngStrap']);
+
+(function () {
+    "use strict";
+    app.config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider){
+        $locationProvider.html5Mode(true); 
+
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/home.html',
+                controller: 'MainCtrl'
+            })
+            .when('/shows/:id', {
+                templateUrl: 'views/detail.html',
+                controller: 'DetailCtrl'
+            })
+            .when('/login', {
+                templateUrl: 'views/login.html',
+                controller: 'LoginCtrl'
+            })
+            .when('/signup', {
+                templateUrl: 'views/signup.html',
+                controller: 'SignupCtrl'
+            })
+            .when('/add', {
+                templateUrl: 'views/add.html',
+                controller: 'AddCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
     
-});
+    }]);
+}());
